2ff789eac05ba125900ec13593f2a5d5e0b268c1
byulika2 <byulika2@gmail.com>
2023-01-11 18:40:51 +0900

    Vue 스터디

---
+++ vue-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form action="">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <button>login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function (){
+    return{
+      username: '',
+      password: '',
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
